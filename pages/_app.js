6315d8c8b799779136c0bd1328298ec1b2cfd044
pakavi <pakavi@protonmail.ch>
2022-04-11 08:28:45 +0300

    Setup moralis database

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,22 @@
 import { RobinhoodProvider } from "../context/RobinhoodContext.js";
+import { MoralisProvider } from "react-moralis";
 
 import "../styles/globals.css";
 
 
+const serverUrl = process.env.MORALIS_SERVER_URL
+const appId = process.env.MORALIS_APP_ID 
+
 function MyApp({ Component, pageProps }) {
   return (
-    <RobinhoodProvider>
-      <Component {...pageProps} />
-    </RobinhoodProvider>
+    <MoralisProvider
+      serverUrl={serverUrl}
+      appId={appId}
+    >
+      <RobinhoodProvider>
+        <Component {...pageProps} />
+      </RobinhoodProvider>
+    </MoralisProvider>
   );
 }
 
