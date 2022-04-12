de6c9d4a7f686fde2b78e78f47624e885378a1f2
pakavi <pakavi@protonmail.ch>
2022-04-12 08:12:43 +0300

    Setup hardhat config w/ smart contract deploy *fixed*

--- smart-contract/hardhat.config.js
+++ smart-contract/hardhat.config.js
@@ -1,21 +1,16 @@
 require("@nomiclabs/hardhat-waffle");
+require("dotenv").config({ path: ".env" });
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
+const url = process.env.NEXT_PUBLIC_ALCHEMY_ETH_URL;
+const walletKey = process.env.NEXT_PUBLIC_WALLET_PRIVATE_KEY;
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: url,
+      accounts: [walletKey],
+    },
+  },
 };
