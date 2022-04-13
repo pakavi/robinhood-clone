4224083a3037bea762147b7d9cf5d5b452c3e1c7
pakavi <pakavi@protonmail.ch>
2022-04-13 08:33:30 +0300

    Added helper functions to context w/ portfolio chart

--- components/BuyTokens.js
+++ components/BuyTokens.js
@@ -1,12 +1,43 @@
-import { styles } from './BuyTokens.styles.js'
+import { useContext } from "react";
+
+import { RobinhoodContext } from "../context/RobinhoodContext.js";
+
+import { styles } from "./BuyTokens.styles.js";
 
 
 const BuyTokens = () => {
+  const {
+    isAuthenticated,
+    setAmount,
+    mint,
+    setCoinSelect,
+    coinSelect,
+    toCoin,
+    amount,
+    setToCoin,
+  } = useContext(RobinhoodContext);
+
   return (
     <form className={styles.formContainer}>
       <div className="flex flex-col h-full w-full items-center">
-        <select className={styles.left}>
+        <select
+          className={styles.left}
+          value={coinSelect}
+          onChange={(e) => setCoinSelect(e.target.value)}
+        >
           <option className={styles.options}>BTC</option>
+          <option className={styles.options}>DOGE</option>
+          <option className={styles.options}>ETH</option>
+          <option className={styles.options}>SOL</option>
+          <option className={styles.options}>USDC</option>
+        </select>
+        <select
+          className={styles.left}
+          value={toCoin}
+          onChange={(e) => setToCoin(e.target.value)}
+        >
+          <option className={styles.options}>BTC</option>
+          <option className={styles.options}>DOGE</option>
           <option className={styles.options}>ETH</option>
           <option className={styles.options}>SOL</option>
           <option className={styles.options}>USDC</option>
@@ -18,8 +49,8 @@ const BuyTokens = () => {
         />
         <button
           className={styles.noticeCTA}
-          type="submit"
-          //   onClick={() => mint()}
+          type="button"
+          onClick={() => mint()}
         >
           Send
         </button>
--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,44 @@
+import { useState, useContext } from 'react';
+import axios from 'axios';
+
+import { RobinhoodContext } from '../context/RobinhoodContext.js';
+
+import PortfolioChart from '../components/PortfolioChart.js';
+import BuyTokens from "../components/BuyTokens.js";
+
+import Header from "../components/Header.js";
+import Notice from "../components/Notice.js";
+import Assets from "../components/Assets.js";
+
 import { BiDotsHorizontalRounded } from "react-icons/bi";
 import { AiOutlinePlus } from "react-icons/ai";
 
-import Header from '../components/Header.js'
-import BuyTokens from '../components/BuyTokens.js'
-import Notice from '../components/Notice.js'
-import Assets from '../components/Assets.js'
-
-import { styles } from './index.styles.js'
+import { styles } from "./index.styles.js";
 
 
-export default function Home() {
+export default function Home({ coins }) {
+  const [myCoins] = useState([...coins.slice(0, 15)]);
+  const { balance } = useContext(RobinhoodContext);
+
   return (
     <div className={styles.wrapper}>
       <Header />
       <div className={styles.mainContainer}>
         <div className={styles.leftMain}>
           <div className={styles.portfolioAmountContainer}>
-            <div className={styles.portfolioAmount}>23 ETH</div>
+            <div className={styles.portfolioAmount}>{balance} ETH</div>
             <div className={styles.portfolioPercent}>
-              +0.0008(+0.57%)
-              {' '}<span className={styles.pastHour}>Past Hour</span>
+              +0.0008(+0.57%) <span className={styles.pastHour}>Past Hour</span>
             </div>
           </div>
           <div>
             <div className={styles.chartContainer}>
-              {/* <PortfolioContainer /> */}
+              <PortfolioChart />
             </div>
           </div>
           <div className={styles.buyingPowerContainer}>
             <div className={styles.buyingPowerTitle}>Buying Power</div>
-            <div className={styles.buyingPowerAmount}>12 ETH</div>
+            <div className={styles.buyingPowerAmount}>{balance} ETH</div>
           </div>
           <div className={styles.notice}>
             <div className={styles.noticeContainer}>
@@ -47,8 +56,12 @@ export default function Home() {
             <div className={styles.ItemTitle}>Crypto Currencies</div>
             <BiDotsHorizontalRounded className={styles.moreOptions} />
           </div>
-          <Assets coin={"BTC"} price={0.89} />
-          <Assets coin={"ETH"} price={0.64} />
+          {myCoins.map((coin) => {
+            let price = parseFloat(coin.price);
+            price = price.toFixed(2);
+
+            return <Assets key={coin.uuid} coin={coin} price={price} />;
+          })}
           <div className={styles.rightMainItem}>
             <div className={styles.ItemTitle}>Lists</div>
             <AiOutlinePlus className={styles.moreOptions} />
@@ -58,3 +71,31 @@ export default function Home() {
     </div>
   );
 }
+
+
+export const getStaticProps = async () => {
+  const options = {
+    method: "GET",
+    url: "https://coinranking1.p.rapidapi.com/coins",
+    params: {
+      referenceCurrencyUuid: "yhjMzLPhuIDl",
+      timePeriod: "24h",
+      tiers: "1",
+      orderBy: "marketCap",
+      orderDirection: "desc",
+      limit: "50",
+      offset: "0",
+    },
+    headers: {
+      "X-RapidAPI-Host": process.env.COIN_RANKING_HOST,
+      "X-RapidAPI-Key": process.env.COIN_RANKING_KEY,
+    },
+  };
+
+  const res = await axios.request(options);
+  const coins = res.data.data.coins;
+
+  return {
+    props: { coins },
+  };
+};
